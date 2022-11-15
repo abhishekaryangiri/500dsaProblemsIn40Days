4309987507e704ac3dac4dc2ccbd8a6975e871d0
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-15 23:37:53 +0530

    java

---
+++ basicProblems/src/dsaSenapati/Basic6PrintJavaPattern.java
@@ -0,0 +1,11 @@
+//Write a Java program to display the following pattern
+package dsaSenapati;
+
+public class Basic6PrintJavaPattern {
+public static void main(String[] args) {
+    System.out.println("      J      a     v         v     a ");
+    System.out.println("      J     a a     v     v     a a");
+    System.out.println(" J    J   aaaaa    V V     aaaaa");
+    System.out.println(" J J J   a        a    V     a        a");
+}
+}
