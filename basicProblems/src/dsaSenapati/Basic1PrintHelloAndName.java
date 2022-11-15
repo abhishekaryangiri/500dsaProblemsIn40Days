da7bf5306014e6183bd5b4114786a58c2e1a55b4
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-15 23:20:31 +0530

    b..

---
+++ basicProblems/src/dsaSenapati/Basic1PrintHelloAndName.java
@@ -0,0 +1,9 @@
+//Print hello and your name on a separate lines
+package dsaSenapati;
+
+public class Basic1PrintHelloAndName {
+public static void main(String[] args) {
+	System.out.println("Hello");
+	System.out.println("Senapati Abhi");
+}
+}
