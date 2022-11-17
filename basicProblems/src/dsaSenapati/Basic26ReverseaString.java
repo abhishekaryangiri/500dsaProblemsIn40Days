5a562ae3e6f8f359f6670e75c88a98a6271a1eae
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-17 23:23:14 +0530

    reverse string

---
+++ basicProblems/src/dsaSenapati/Basic26ReverseaString.java
@@ -0,0 +1,24 @@
+/*
+ Write a Java program to reverse a string
+Input Data:
+Input a string: The quick brown fox
+Expected Output
+
+Reverse string: xof nworb kciuq ehT
+ */
+package dsaSenapati;
+
+import java.util.Scanner;
+
+public class Basic26ReverseaString {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        char[] letters = scanner.nextLine().toCharArray();
+        System.out.print("Reverse string: ");
+        for (int i = letters.length - 1; i >= 0; i--) {
+            System.out.print(letters[i]);
+        }
+        System.out.print("\n");
+    }
+}
