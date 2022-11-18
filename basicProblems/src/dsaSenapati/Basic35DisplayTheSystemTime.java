9f0ba74c812946882ba2f2b3db789951f17971f7
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-18 23:24:01 +0530

    display the sys

---
+++ basicProblems/src/dsaSenapati/Basic35DisplayTheSystemTime.java
@@ -0,0 +1,21 @@
+/*
+  Write a Java program to display the system time. Go to the editor
+Sample Output:
+
+Current Date time: Fri Jun 16 14:17:40 IST 2017 
+ */
+package dsaSenapati;
+
+import java.util.Scanner;
+
+public class Basic35DisplayTheSystemTime {
+	public static void main(String[] args) {
+
+		  int n;
+		  char s1, s2, s3;
+		  Scanner in = new Scanner(System.in);
+		  System.out.print("Input number: ");
+		  n = in .nextInt();
+		  System.out.printf("%d + %d%d  + %d%d%d\n", n, n, n, n, n, n);
+		 }
+}
