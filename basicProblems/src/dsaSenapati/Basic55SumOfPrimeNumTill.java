3bc837cf707ce4a771ed21f1a7fc645a0a7cdaed
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-19 23:22:18 +0530

    sum..

---
+++ basicProblems/src/dsaSenapati/Basic55SumOfPrimeNumTill.java
@@ -0,0 +1,37 @@
+/*
+  Write a Java program to compute the sum of the first 50 prime numbers. 
+Sample Output:
+
+Sum of the first 50 prime numbers: 24133 
+ */
+package dsaSenapati;
+
+public class Basic55SumOfPrimeNumTill {
+	 public static void main(String[] args)
+	 {
+	    int sum = 1;
+		 int ctr = 0; 
+		 int n = 0; 
+	    	
+			while (ctr < 100) {
+				n++; 
+				if (n % 2 != 0) { 
+				// check if the number is even
+					if (is_Prime(n)) {
+						sum += n; 					
+					}
+				}
+	                    ctr++; 	
+			}
+			System.out.println("\nSum of the prime numbers till 50: "+sum); 	
+		 }
+		
+	   	public static boolean is_Prime(int n) {
+			for (int i = 3; i * i <= n; i+= 2) {
+				if (n % i == 0) {
+					return false; 
+				}
+			}
+			return true;
+		}
+}
