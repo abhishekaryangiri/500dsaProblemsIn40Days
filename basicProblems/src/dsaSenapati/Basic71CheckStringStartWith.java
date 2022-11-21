eb38a8575583eabe46f5c83b27e24c522c1e31db
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-21 23:31:34 +0530

    check

---
+++ basicProblems/src/dsaSenapati/Basic71CheckStringStartWith.java
@@ -0,0 +1,16 @@
+/*
+ Write a Java program to check if a string starts with a specified word. 
+Sample Data: string1 = "Hello how are you?"
+Sample Output:
+
+true
+ */
+package dsaSenapati;
+
+public class Basic71CheckStringStartWith {
+	 public static void main(String[] args)
+	 {
+	   String string1 = "Hello how are you?";
+	    System.out.println(string1.startsWith("Hello"));
+	  }
+}
