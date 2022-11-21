0f92f2879bfb09fd8191083a06524efd34566318
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-21 23:50:34 +0530

    curr

---
+++ basicProblems/src/dsaSenapati/Basic75CurrApp.java
@@ -0,0 +1,12 @@
+/*
+ Write a Java program to check whether a security manager has already been established for the current application or not.
+ */
+package dsaSenapati;
+
+public class Basic75CurrApp {
+	public static void main(String[] args)
+	 {
+		System.out.println("System security interface:");
+	    System.out.println(System.getSecurityManager());	 
+	  }
+}
