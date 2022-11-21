b28e2a285d2556bc21ed2d0ea9be06bbb5aeffd9
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-21 23:44:50 +0530

    write

--- basicProblems/src/dsaSenapati/Basic72RepProcc.java
+++ basicProblems/src/dsaSenapati/Basic72RepProcc.java
@@ -1,5 +1,24 @@
+/*
+ Program to start with an integer n, divide n by 2 if n is even or multiply by 3 and add 1 if n is odd, repeat the process until n = 1
+ */
 package dsaSenapati;
 
-public class Basic72RepProcc {
+import java.util.Scanner;
 
+public class Basic72RepProcc {
+	public static void main(String[] args) {
+		System.out.println("Input the value of n: ");
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		while (n != 1) {
+			if (n % 2 == 0) {
+				n = n / 2;				
+			}
+			else {
+				n = (3 * n + 1) / 2;				
+			}
+		}
+		System.out.println("\nValue of n = "+n);
+		in.close();
+	}
 }
