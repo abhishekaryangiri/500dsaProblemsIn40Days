e59cdbd7365c177baf39c6b831311f5f17b9237b
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-22 23:48:51 +0530

    rearrange

--- basicProblems/src/dsaSenapati/Basic88TestIntegCoun.java
+++ basicProblems/src/dsaSenapati/Basic88TestIntegCoun.java
@@ -1,5 +1,30 @@
+/*
+ Write a Java program to test if an array of integers contains an element 10 next to 10 or an element 20 next to 20, but not both
+ */
 package dsaSenapati;
 
-public class Basic88TestIntegCoun {
+import java.util.Arrays;
 
+public class Basic88TestIntegCoun {
+	 public static void main(String[] args)
+	 {
+	    //int[] nums = {10, 10, 2, 4, 9};
+		int[] nums = {10, 10, 2, 4, 20, 20};  
+		int ctr_even = 0, ctr_odd = 0;
+		System.out.println("Original Array: "+Arrays.toString(nums)); 
+		    
+	    boolean found1010 = false;
+	    boolean found2020 = false;
+	      
+	    for(int i = 0; i < nums.length - 1; i++) {
+	        if(nums[i] == 10 && nums[i+1] == 10)
+	            found1010 = true;
+	                        
+	        if(nums[i] == 20 && nums[i+1] == 20)
+	            found2020 = true;
+	    }
+	   
+		System.out.printf( String.valueOf(found1010 != found2020));	
+		System.out.printf("\n");
+	  }
 }
