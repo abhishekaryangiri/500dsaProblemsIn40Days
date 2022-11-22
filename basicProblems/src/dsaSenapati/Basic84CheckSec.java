6526af99627d38d6dc854dceccf8e2f7a15a7006
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-22 23:37:02 +0530

    check sec

---
+++ basicProblems/src/dsaSenapati/Basic84CheckSec.java
@@ -0,0 +1,13 @@
+/*
+ Write a Java program to check whether a security manager has already been established for the current application or not
+ 
+ */
+package dsaSenapati;
+
+public class Basic84CheckSec {
+	public static void main(String[] args)
+	 {
+		System.out.println("System security interface:");
+	    System.out.println(System.getSecurityManager());	 
+	  }
+}
