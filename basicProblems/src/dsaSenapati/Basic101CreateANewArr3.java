908bf27d6dffbad8a91d438afd9da01d74c2365a
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-23 23:50:18 +0530

    create a arr3

---
+++ basicProblems/src/dsaSenapati/Basic101CreateANewArr3.java
@@ -0,0 +1,25 @@
+/*
+ Write a Java program to create a new array that is left shifted from a given array of integers
+ */
+package dsaSenapati;
+
+import java.util.Arrays;
+
+public class Basic101CreateANewArr3 {
+	public static void main(String[] args)
+	 {
+		int[] array_nums = {11, 15, 13, 10, 45, 20};
+	    System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+		
+		if(array_nums.length >1)
+	    {          
+	    int first = array_nums[0];
+	              
+	    for(int i = 1; i < array_nums.length; i++)
+	        array_nums[i - 1] = array_nums[i];
+	                        
+	    array_nums[array_nums.length - 1] = first;
+		System.out.println("New Array: "+Arrays.toString(array_nums)); 
+		}
+	  }
+}
