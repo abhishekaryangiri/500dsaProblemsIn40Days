b0dcd0504457c54814c6bec25145eacc425babad
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-11-24 23:31:42 +0530

    form..

--- basicProblems/src/dsaSenapati/Basic102CheckAnArrOfInt.java
+++ basicProblems/src/dsaSenapati/Basic102CheckAnArrOfInt.java
@@ -1,5 +1,30 @@
+/*
+ Check if an array of integers contains three increasing adjacent numbers
+ */
 package dsaSenapati;
 
-public class Basic102CheckAnArrOfInt {
+import java.util.Arrays;
 
+public class Basic102CheckAnArrOfInt {
+	public static void main(String[] args)
+	 {
+		int[] array_nums = {11, 12, 13, 14, 45, 20};
+	    System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+		int result =1;
+		
+	      for(int i = 0; i <= array_nums.length - 3; i++) {
+	        if(array_nums[i] + 1 == array_nums[i+1] && array_nums[i+1] + 1 == array_nums[i+2])
+	            result =0;
+	    }
+	                
+		if (result==1)
+		 {
+		  System.out.printf(String.valueOf(false));
+		 }
+		else
+		 {
+		   System.out.printf(String.valueOf(true));
+		 }
+		 System.out.printf("\n");
+	 }
 }
--- basicProblems/src/dsaSenapati/Basic103AddAllDigit.java
+++ basicProblems/src/dsaSenapati/Basic103AddAllDigit.java
@@ -1,5 +1,19 @@
+/*
+ Write a Java program to add all the digits of a given positive integer until the result has a single digit
+ */
 package dsaSenapati;
 
-public class Basic103AddAllDigit {
+import java.util.Scanner;
 
+public class Basic103AddAllDigit {
+	 public static void main(String[] arg) {	
+	        Scanner in = new Scanner(System.in);	
+	       System.out.print("Input a positive integer: ");
+	        int n = in.nextInt(); 
+			if (n>0)
+			{
+	           System.out.print("The single digit number is: "+(n == 0 ? 0 : (n % 9 == 0 ? 9 : n % 9)));
+			}
+			System.out.println("\n");  
+	    }
 }
